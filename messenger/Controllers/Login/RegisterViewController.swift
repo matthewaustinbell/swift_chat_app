c58543d9d5ee694b70b7c280e3e4d272fc6db14b
Matt Bell <matthewaustinbell@gmail.com>
2020-12-22 14:28:48 -0600

    added access photo for user profile

--- messenger/Controllers/Login/RegisterViewController.swift
+++ messenger/Controllers/Login/RegisterViewController.swift
@@ -132,7 +132,7 @@ class RegisterViewController: UIViewController {
     }
     
     @objc private func didTapChangeProfilePic() {
-        print("change pic called ")
+        presentPhotoActionSheet()
     }
     
     override func viewDidLayoutSubviews() {
@@ -225,4 +225,35 @@ extension RegisterViewController: UITextFieldDelegate {
     }
 }
 
-
+extension RegisterViewController: UIImagePickerControllerDelegate {
+    
+    func presentPhotoActionSheet() {
+            let actionSheet = UIAlertController(title: "Profile Picture",
+                                                message: "How would you like to select a picture?",
+                                                preferredStyle: .actionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel",
+                                            style: .cancel,
+                                            handler: nil))
+        actionSheet.addAction(UIAlertAction(title: "Take Photo",
+                                            style: .default,
+                                            handler: { _ in
+                                                
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Chose Photo",
+                                            style: .default,
+                                            handler: { _ in
+        }))
+                                            
+        present(actionSheet, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        
+    }
+    
+}
