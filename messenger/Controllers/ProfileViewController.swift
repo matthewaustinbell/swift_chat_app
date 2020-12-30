6149202db4a3aefc7ba1c8284db71bb5ef40d3f9
Matt Bell <matthewaustinbell@gmail.com>
2020-12-30 08:59:29 -0600

    commiting changes xcode is malfunctioning as to not lose progress

--- messenger/Controllers/ProfileViewController.swift
+++ messenger/Controllers/ProfileViewController.swift
@@ -6,24 +6,53 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet var tabelView: UITableView!
+    
+    let data = ["Log Out"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tabelView.register(UITableViewCell.self,
+                           forCellReuseIdentifier: "cell")
+        tabelView.delegate = self
+        tabelView.dataSource = self
+    }
+ 
+}
 
-        // Do any additional setup after loading the view.
+
+extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tabelView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row]
+        cell.textLabel?.textAlignment = .center
+        cell.textLabel?.textColor = .red
+        return cell
     }
-    */
-
+    
+     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tabelView.deselectRow(at: indexPath, animated: true)
+        
+        do{
+            try FirebaseAuth.Auth.auth().signOut()
+            
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false)
+            
+        }
+        catch{
+            print("failed to log out")
+        }
+    }
 }
